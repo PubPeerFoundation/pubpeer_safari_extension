c39e5a13563aaa2fa396fb63f19ce16a374e2fa3
rogiermuijen <rogiermuijen@gmail.com>
2019-10-28 10:13:55 +0100

    Improve the UI

--- Pubpeer Safari App Extension/SafariExtensionViewController.swift
+++ Pubpeer Safari App Extension/SafariExtensionViewController.swift
@@ -21,14 +21,37 @@ class SafariExtensionViewController: SFSafariExtensionViewController {
     
     static let shared: SafariExtensionViewController = {
         let shared = SafariExtensionViewController()
-        shared.preferredContentSize = NSSize(width:385, height:285)
+        DispatchQueue.main.async {
+            shared.preferredContentSize = NSSize(width:385, height:285)
+        }
+        
         return shared
     }()
+    
+    private func _style(title: String?, value: Any, isBold: Bool = false) -> NSMutableAttributedString {
+        let attributedString = NSMutableAttributedString(string: title ?? "")
+        attributedString.addAttribute(NSAttributedString.Key.kern, value: value, range: NSMakeRange(0, title?.count ?? 0))
+        if (isBold) {
+            attributedString.addAttribute(NSAttributedString.Key.font, value: NSFont(name: "Helvetica-Bold", size: 16)!, range: NSMakeRange(3, (title?.count ?? 0) - 4))
+        }
+        let paragraph = NSMutableParagraphStyle()
+        paragraph.alignment = .center
+        attributedString.addAttribute(NSAttributedString.Key.paragraphStyle, value: paragraph, range: NSMakeRange(0, title?.count ?? 0))
+        return attributedString
+    }
+    
     
     func initUIElements() {
         self.btnDisableOnce.layer?.backgroundColor = NSColor.init(calibratedRed: 106/255, green: 194/255, blue: 188/255, alpha: 1).cgColor
         self.btnDisableForever.layer?.backgroundColor = NSColor.init(calibratedRed: 106/255, green: 194/255, blue: 188/255, alpha: 1).cgColor
-        self.txtDescription_2.stringValue = "on \(DisabledHostsManager.shared.normalizeUrl( self.url ?? "" ))?"
+        self.btnDisableOnce.layer?.cornerRadius = 5
+        self.btnDisableForever.layer?.cornerRadius = 5
+        self.txtLogo.attributedStringValue = self._style(title: "PUBPEER", value: 2)
+        self.txtDescription_1.attributedStringValue = self._style(title: "Disable Pubpeer extension", value: 1)
+        self.txtDescription_2.attributedStringValue = self._style(title: "on \(DisabledHostsManager.shared.normalizeUrl( self.url ?? "" ))?", value: 1, isBold: true)
+        self.btnDisableOnce.attributedTitle = self._style(title: "DISABLE ON THIS SITE UNTIL NEXT VISIT", value: 0.4)
+        self.btnDisableForever.attributedTitle = self._style(title: "DISABLE ON THIS SITE FOREVER", value: 0.4)
+        self.btnEnable.attributedTitle = self._style(title: "ENABLE ON THIS SITE", value: 0.4)
     }
     
     func setActivatedURL(with url: String?) {
