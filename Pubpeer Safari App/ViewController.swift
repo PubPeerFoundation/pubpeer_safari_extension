4007ce8f61a7b4df35e181a8138d6750d5e3ddf1
Rogier Muijen <rogiermuijen@gmail.com>
2019-12-02 18:12:43 +0100

    Adjustment for app store submission

--- Pubpeer Safari App/ViewController.swift
+++ Pubpeer Safari App/ViewController.swift
@@ -19,7 +19,7 @@ class ViewController: NSViewController {
     }
     
     @IBAction func openSafariExtensionPreferences(_ sender: AnyObject?) {
-        SFSafariApplication.showPreferencesForExtension(withIdentifier: "com.pubpeer.Pubpeer-Safari-App-Extension") { error in
+        SFSafariApplication.showPreferencesForExtension(withIdentifier: "com.pubpeer.Pubpeer-Safari-App.Extension") { error in
             if let _ = error {
                 // Insert code to inform the user that something went wrong.
 
